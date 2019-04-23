b9d52f50e776cab0c482690c8331f5c545e1a1c8
Thomas Pratama Putra <thomas.putra@binus.ac.id>
2019-04-23 08:26:09 +0700

    Initial Commit

---
+++ Frudder!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frudder!
+//
+//  Created by Thomas Pratama Putra on 23/04/19.
+//  Copyright © 2019 NA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
